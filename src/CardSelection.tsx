205a444403b0b01102761f046d08eaf337f915c0
Corentin Couderc <corentincouderc@free.fr>
2026-04-01 19:35:02 +0200

    Added card selection display

---
+++ src/CardSelection.tsx
@@ -0,0 +1,29 @@
+import "./CardSelection.css"
+import type {CardData} from "./cards.ts";
+import Card from "./Card.tsx";
+
+interface CardSelectionProps {
+    levelReached: number;
+    cards: CardData[];
+    selectCard: (card: CardData) => void;
+}
+
+function CardSelection(props: CardSelectionProps) {
+
+    return (
+        <div className={"card-selection"}>
+            <h1 id={"levelReachedText"}>Niveau {props.levelReached}</h1>
+            <h2 id={"selectCardText"}>Sélectionnez une carte :</h2>
+
+            <div className={"card-list"}>
+                {
+                    props.cards.map((card: CardData, index: number) => (
+                         <Card key={index} card={card} onCardClicked={props.selectCard} shouldShowBack={false} isCardSelected={false}/>
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
+
+export default CardSelection;
--- src/Level.tsx
+++ src/Level.tsx
@@ -1,123 +1,48 @@
 import "./Level.css"
 import {useEffect, useState} from 'react';
-import Card from "./Card.tsx";
-import {type CardData, ECardCategory, blankCard, allCards, type CardCategory, orderedCategories} from './cards';
-import {allDecks, type LevelData} from "./levels.ts";
+import {type CardData, ECardCategory, allCards, type CardCategory, orderedCategories} from './cards';
+import {type LevelData} from "./levels.ts";
 import CardSlot from "./CardSlot.tsx";
+import CardSelection from "./CardSelection.tsx";
 
 interface LevelProps {
     levelData: LevelData;
 }
 
 export function Level(props: LevelProps) {
-    const levelIndex = props.levelData.level - 1;
-    const [levelDeck, setLevelDeck] = useState<CardData[]>([]);
+    // const numberOfRerolls = 5;
+    // const [rerolls, setRerolls] = useState<number>(numberOfRerolls);
 
-    const cardsPerTurn = 3;
-    const numberOfDiscards = 5;
     const [hasGameStarted, setHasGameStarted] = useState(false);
     const [isGameOver, setIsGameOver] = useState(false);
-    const [isLevelFinished, setIsLevelFinished] = useState(false);
+    const [isSelectingCard, setIsSelectingCard] = useState(true);
     const [energy, setEnergy] = useState(0);
     const [currentXP, setCurrentXP] = useState(0);
     const [currentLevel, setCurrentLevel] = useState(0);
-    const [discards, setDiscards] = useState<number>(numberOfDiscards);
-    const [hand, setHand] = useState<CardData[]>([]);
-    const [deck, setDeck] = useState<CardData[]>([]);
     const initialBoard: (CardData | null)[] = [null, null, null, null, null, null];
     const [boardCards, setBoardCards] = useState<(CardData | null)[]>(initialBoard);
 
-
-
-    // Fill levelDeck & shuffle on mount
+    // Call tick() method every second
     useEffect(() => {
-        let newLevelDeck: CardData[] = [];
-        for (let i = 0; i < allDecks[levelIndex].cardIds.length; i++) {
-            let cardToAdd = allCards.find((card) => card.id === allDecks[levelIndex].cardIds[i]);
-            if (!cardToAdd) {
-                console.error("Card index of level not found");
-                break;
-            }
-            newLevelDeck.push(cardToAdd);
+        // Game has begun if there is at least one card on the board
+        if (hasGameStarted && !isSelectingCard && !isGameOver) {
+            const interval = setInterval(() => tick(), 1000);
+            return () => clearInterval(interval);
         }
-        newLevelDeck = shuffle(newLevelDeck);
-        setLevelDeck(newLevelDeck);
-    }, []);
+    }, [hasGameStarted, energy, isSelectingCard]);
 
-    useEffect(() => {
-        setHand(levelDeck.slice(0, cardsPerTurn));
-        setDeck(levelDeck.slice(cardsPerTurn));
-    }, [levelDeck]);
-
-    function shuffle<T>(array: T[]) {
-        const sortedArr = structuredClone(array);
-        for (let i = sortedArr.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [sortedArr[i], sortedArr[j]] = [sortedArr[j], sortedArr[i]];
-        }
-        return sortedArr;
-    }
-
-    function hasCardForCategory(deck: CardData[], category: CardCategory) {
-        return deck.filter(c => c.category === category).length > 0;
-    }
-
-    function getCategoryIndex(category: CardCategory) {
-        if (category === ECardCategory.NONE)
-            return -1;
-
-        return orderedCategories.indexOf(category);
-    }
-
-    const playCard = (card: CardData) => {
-        selectCard(card); // temporary
-
-        const newHand = hand.filter(c => c.id !== card.id);
-        setHand(newHand);
-
-        // Add card to played cards
-        const newBoardCards = [...boardCards];
-        newBoardCards[getCategoryIndex(card.category)] = card;
-        setBoardCards(newBoardCards);
-    };
-
-    const discardCard = (card: CardData) => {
-        if (discards <= 0) return;
-
-        // Remove card from hand
-        let newHand = hand.filter(c => c.id !== card.id);
-        if (deck.length !== 0) {
-            // Replace it if deck is not empty
-            const newCard = deck[0];
-            setDeck(deck.slice(1));
-            newHand.push(newCard);
-        }
-        setHand(newHand);
-        setDiscards(discards - 1);
-    }
-
-    // useEffect(() => {
-    //     if (hand.length == 0) { // When all cards have been played, auto end turn
-    //         endTurn();
-    //     }
-    // }, [hand]);
-
-    const endTurn = () => {
-        // updateScore(); deprecated
-
-        if (deck.length === 0)
-        {
-            setIsLevelFinished(true);
+    function tick() {
+        if (energy == 0) {
+            setIsGameOver(true);
             return;
         }
 
-        const cardsToDraw = cardsPerTurn - hand.length;
-        setHand(hand.concat(deck.slice(0, cardsToDraw)));
-        setDeck(deck.slice(cardsToDraw));
-    };
+        // Decrement energy 1 per tick call & add XP
+        addEnergy(-1);
+        const xpPerTick = computeXPPerTick();
+        addXP(xpPerTick);
+    }
 
-
-    // New implementation
     function addEnergy(energyToAdd: number) {
         setEnergy(energy + energyToAdd);
     }
@@ -143,34 +68,51 @@ export function Level(props: LevelProps) {
         return totalXpPerTick;
     }
 
-    function tick() {
-        if (energy == 0) {
-            setIsGameOver(true);
-            return;
-        }
 
-        // Decrement energy 1 per tick call & add XP
-        addEnergy(-1);
-        const xpPerTick = computeXPPerTick();
-        addXP(xpPerTick);
-    }
-    // Call tick() method every second
-    useEffect(() => {
-        // Game has begun if there is at least one card on the board
-        if (hasGameStarted && !isGameOver) {
-            const interval = setInterval(() => tick(), 1000);
-            return () => clearInterval(interval);
-        }
-    }, [hasGameStarted, energy]);
 
     function levelUp() {
         setCurrentLevel(currentLevel + 1);
-        cardSelection();
+        displayCardSelection();
     }
 
-    function cardSelection() {
-        // TODO : display 3 cards to select from
-        selectCard(allCards[1]);
+    function displayCardSelection() {
+        setIsSelectingCard(true);
+    }
+
+    function pickRandomCardsToDisplay() {
+        if (!hasGameStarted) {
+            return [allCards[0], allCards[0], allCards[0]];
+        }
+        else {
+            return [allCards[Math.floor(Math.random() * allCards.length)], allCards[Math.floor(Math.random() * allCards.length)], allCards[Math.floor(Math.random() * allCards.length)]];
+
+            // TODO : implement better card selection algorithm (no redraw of same card, priority in randomness etc..)
+            // Start from something like this :
+            /*
+            let newLevelDeck: CardData[] = [];
+            for (let i = 0; i < allDecks[levelIndex].cardIds.length; i++) {
+                let cardToAdd = allCards.find((card) => card.id === allDecks[levelIndex].cardIds[i]);
+                if (!cardToAdd) {
+                    console.error("Card index of level not found");
+                    break;
+                }
+                newLevelDeck.push(cardToAdd);
+            }
+             */
+        }
+    }
+
+    function getCategoryIndex(category: CardCategory) {
+        if (category === ECardCategory.NONE)
+            return -1;
+
+        return orderedCategories.indexOf(category);
+    }
+
+    function addCardToBoard(card: CardData) {
+        const newBoardCards = [...boardCards];
+        newBoardCards[getCategoryIndex(card.category)] = card;
+        setBoardCards(newBoardCards);
     }
 
     function selectCard(card: CardData) {
@@ -179,10 +121,17 @@ export function Level(props: LevelProps) {
         }
         addEnergy(card.effects.energyFlat);
         addXP(card.effects.xpFlat);
+        addCardToBoard(card);
+
+        setIsSelectingCard(false);
     }
 
     return (
         <div className="game-hud">
+            {
+                isSelectingCard ? <CardSelection levelReached={currentLevel} cards={pickRandomCardsToDisplay()} selectCard={selectCard}/> : null
+            }
+
             <div className="game-header">
                 <h1>Level: {currentLevel} {props.levelData.title} | Exp: {currentXP}/10 | Energy: {energy}</h1>
             </div>
